eccc8ba7619bb289d6a40c47a68bdfaee300d80b
manoj <manojpoojary2004@gmail.com>
2025-03-21 18:03:57 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import VideoSearch from './component/VideoSearch';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <VideoSearch/>
     </div>
   );
